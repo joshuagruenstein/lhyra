07bfd4fc908f308b94ce30ac21b88e9520dafb51
Benjamin Spector <benjaminfspector@gmail.com>
2019-04-27 12:17:36 -0400

    Added feature extractor for sort

--- sorting.py
+++ sorting.py
@@ -1,5 +1,23 @@
 from typing import Any, Callable, Dict, List
+from lhyra import Solver, FeatureExtractor
+from math import log2
 
+class SortFeatureExtractor(FeatureExtractor):
+    @property
+    def shape(self) -> List[int]:
+        """
+        Get the output shape of the feature extractor.
+        :return: A list of integers representing the output's dimensions.
+        """
+        return [1] # Length, (mean, variance)?
+
+    def __call__(self, data: Any) -> List:
+        """
+        Call extractor on given data.
+        :param data: A piece of data to extract the parameters of.
+        :return: Floats between 0 and 1 of shape self.shape.
+        """
+        return 1/(log2(len(data))+1)
 
 def merge_sort(data: List, hook: Callable, _: Dict[str, Any]) -> List:
     """
